0a2667752e75de6c1f9a13d5a54138e5f0e03ab8
yc <arigatouyc@foxmail.com>
2023-06-28 20:19:48 +0800

    0628

--- src/main.ts
+++ src/main.ts
@@ -4,7 +4,9 @@ import { createPinia } from 'pinia'
 import piniaPluginPersistedstate from 'pinia-plugin-persistedstate'
 import App from './App.vue'
 import router from './router'
+
 import '@/styles/common.scss'
+
 import { lazyPlugin } from '@/directives'
 import { componentPlugin } from '@/components'
 
