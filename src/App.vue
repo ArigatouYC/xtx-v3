dedd675683a5428a4641eb2857822b69b6d51ff5
caiyimin <arigatouyc@foxmail.com>
2023-08-20 15:48:08 +0800

    11

--- src/App.vue
+++ src/App.vue
@@ -1,19 +1,26 @@
 <script setup lang="ts">
 
-import { onMounted } from 'vue'
+import { onMounted, ref } from 'vue'
 import { useCounterStore } from "@/stores/navData";
+import axios from 'axios';
 
 let navData = useCounterStore()
 
 onMounted(() => {
   navData.getCategory()
+
+  let res = ref('')
+  let test = async function test() {
+    res = await axios.get('https://www.fastmock.site/mock/ae0543719133bfde880d4502d1958449/demo/api/test')
+    console.log(res);
+  }
+  test()
 })
 
 </script>
 
 <template>
   <RouterView />
-
 </template>
 
 <style scoped></style>
